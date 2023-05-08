30de3026e5a27d2995349701dbe80dbcc9dfa7df
BossKeeg <keeg.hamburgh@gmail.com>
2023-05-07 21:24:07 -0700

    Test Triangle
    Test

---
+++ Fighting Game/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,259 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject otherPlayer;
+
+    public KeyCode playerDodge;
+    public KeyCode playerAttack;
+
+    public float forceMultiplierWait = 2f;
+    public float knockBackWait = 2f;
+
+    bool canInteract;
+
+    public bool isFlipped;
+
+    public Vector2 originalForce;
+    public Vector2 originalForceFlipped;
+    public Vector2 attackForce;
+
+    public Animator animator;
+
+    public string shieldName;
+    public string stopShieldName;
+
+    public float maxHP = 100f;
+    public float currentHP;
+    public float damage = 10f;
+    public Transform healthBar;
+
+    public float dodgeCooldown = 2f;
+    public List<GameObject> blowsIcon;
+    public int maxBlowCount;
+    public int currentBlowCount;
+
+    public bool isAttack;
+    public bool isShiled;
+
+    private void Awake()
+    {
+        originalForce = this.GetComponent<ConstantForce2D>().force;
+        originalForceFlipped = this.GetComponent<ConstantForce2D>().force * -1;
+
+        currentHP = maxHP;
+
+        Vector3 size = Vector3.one;
+        size.y = healthBar.localScale.y;
+
+        healthBar.localScale = size;
+
+        currentBlowCount = maxBlowCount;
+    }
+     
+    private void Update()
+    {
+        if (!isAttack)
+        {
+            if (isFlipped)
+            {
+                this.GetComponent<ConstantForce2D>().force = originalForceFlipped;
+            }
+            else
+            {
+                this.GetComponent<ConstantForce2D>().force = originalForce;
+            }
+        }
+
+        if (isAttack)
+        {
+            this.GetComponent<ConstantForce2D>().force = attackForce * otherPlayer.transform.forward.z /** (otherPlayer.GetComponent<PlayerManager>().isFlipped ? 1 : -1)*/;
+        }
+
+        //if (Input.GetKeyUp(playerDodge))
+        //{
+        //    Dodge();
+        //}
+
+        if (Input.GetKeyDown(playerDodge))
+        {
+            Dodge(true);
+        }
+        else if (Input.GetKeyUp(playerDodge))
+        {
+            Dodge(false);
+        }
+
+        if (Input.GetKeyDown(playerAttack))
+        {
+            Attack();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            canInteract = false;
+        }
+
+    }
+
+    public void Damage(float damage)
+    {
+        if (isShiled)
+        {
+            currentBlowCount--;
+
+            if (blowsIcon.Count > 0)
+            {
+                blowsIcon[currentBlowCount].SetActive(false);
+            }
+
+            if (currentBlowCount <= 0)
+            {
+                StartCoroutine(CoolDown());
+                Dodge(false);
+            }
+        }
+        else
+        {
+            StartCoroutine(TakeDamage());
+            currentHP -= damage;
+            
+            Vector3 size = Vector3.one;
+            size.x = currentHP / maxHP;
+            size.y = healthBar.localScale.y;
+
+            healthBar.localScale = size;
+        }
+    }
+
+    void Dodge(bool val)
+    {
+        if (val == isShiled)
+        {
+            return;
+        }
+
+        Debug.Log("Dodging...");
+        StartCoroutine(Dodges(val));
+    }
+
+    void Dodge() 
+    {
+        Debug.Log("Dodging...");
+        StartCoroutine(Dodges());   
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(dodgeCooldown);
+        currentBlowCount = maxBlowCount;
+
+        if (blowsIcon.Count > 0)
+        {
+            foreach (var item in blowsIcon)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    IEnumerator Dodges(bool val)
+    {
+        if (currentBlowCount <= 0 && !isShiled) 
+        {
+            yield break;
+        } 
+
+        isShiled = val;
+
+        animator.SetTrigger(isShiled ? shieldName : stopShieldName);
+
+        if (!isShiled)
+        {
+            yield break;
+        }
+
+        yield return null;
+
+        //this.GetComponent<ConstantForce2D>().enabled = true;
+        //yield return new WaitForSeconds(forceMultiplierWait);
+        //this.GetComponent<ConstantForce2D>().enabled = false;
+    }
+
+    IEnumerator Dodges()
+    {
+        if (currentBlowCount <= 0 && !isShiled)
+        {
+            yield break;
+        }
+
+        isShiled = !isShiled;
+
+        animator.SetTrigger(isShiled ? shieldName : stopShieldName);
+
+        if (!isShiled)
+        {
+            yield break;
+        }
+
+        yield return null;
+
+        //this.GetComponent<ConstantForce2D>().enabled = true;
+        //yield return new WaitForSeconds(forceMultiplierWait);
+        //this.GetComponent<ConstantForce2D>().enabled = false;
+    }
+
+    IEnumerator AttackN()
+    {
+        otherPlayer.GetComponent<PlayerManager>().isAttack = true;
+        otherPlayer.GetComponent<ConstantForce2D>().enabled = true;
+        yield return new WaitForSeconds(knockBackWait);
+        otherPlayer.GetComponent<ConstantForce2D>().enabled = false;
+        otherPlayer.GetComponent<PlayerManager>().isAttack = false;
+    }
+
+    IEnumerator KnockBackThis()
+    {
+        isAttack = true;
+        this.GetComponent<ConstantForce2D>().enabled = true;
+        yield return new WaitForSeconds(knockBackWait);
+        this.GetComponent<ConstantForce2D>().enabled = false;
+        isAttack = false;
+    }
+
+    void Attack()
+    {
+        if (canInteract)
+        {
+            StartCoroutine(AttackN());
+            otherPlayer.GetComponent<PlayerManager>().Damage(damage);
+
+            if (otherPlayer.GetComponent<PlayerManager>().isShiled)
+            {
+                //Knockback this
+                StartCoroutine(KnockBackThis());
+            }
+        }
+    }
+
+    IEnumerator TakeDamage()
+    {
+        animator.SetBool("takenDamage", true);
+        yield return new WaitForSeconds(0.15f);
+        animator.SetBool("takenDamage", false);
+
+    }
+}
---
+++ Fighting Game/Assets/Scripts/PlayerMovementArrows.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovementArrows : MonoBehaviour
+{
+    [Header("Move")]
+    public float moveSpeed = 5f;
+    public float shieldSpeed = 2f;
+
+    [Header("Jump")]
+    public float jumpForce = 10f;
+    public float jumpTime = 0.5f;
+
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
+    private float jumpTimeCounter;
+    private float jumpForceMultiplier = 1f;
+
+    public Transform feetPosition;
+    public float checkRadius = 0.1f;
+    public LayerMask whatIsGround;
+
+    // Add this variable to set the delay before attacking
+    private float attackDelay = 0.25f;
+    private float lastAttackTime = 0f;
+
+    // Add a circle collider and a trigger for attack detection
+    public CircleCollider2D attackCollider;
+    public Transform attackPoint;
+
+    public PlayerManager playerManager;
+
+    void Start()
+    {
+        playerManager = this.GetComponent<PlayerManager>();
+
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Set the circle collider to trigger mode
+        attackCollider.isTrigger = true;
+    }
+
+    void Update()
+    {
+        if (playerManager.currentHP <= 0)
+        {
+            StartCoroutine(IsDead());
+        }
+
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, checkRadius, whatIsGround);
+
+        float horizontal = Input.GetAxisRaw("HorizontalJKI");
+        rb.velocity = new Vector2(horizontal * (playerManager.isShiled ? shieldSpeed : moveSpeed), rb.velocity.y);
+
+        if (isGrounded)
+        { 
+            anim.SetBool("isRunning", horizontal != 0);
+            if (Input.GetKeyDown(KeyCode.O) && isGrounded && Time.time - lastAttackTime >= attackDelay)
+            {
+                lastAttackTime = Time.time; // set last attack time
+                anim.SetBool("isRunning", false);
+                anim.SetTrigger("isAttacking");
+
+                // Enable the circle collider to detect attacks
+                attackCollider.enabled = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.I) && isGrounded && !playerManager.isShiled)
+            {
+                anim.SetBool("isJumping", true);
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                isGrounded = false;
+                anim.SetBool("isRunning", false);
+                anim.SetTrigger("isJumping");
+
+                jumpTimeCounter = jumpTime;
+            }
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+
+            if (Input.GetKey(KeyCode.I) && jumpTimeCounter > 0)
+            {
+                jumpTimeCounter -= Time.deltaTime;
+
+                jumpForceMultiplier = 1f + (jumpTime - jumpTimeCounter) / jumpTime * (jumpForceMultiplier - 1f);
+
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce * jumpForceMultiplier);
+            }
+            else
+            {
+                jumpTimeCounter = 0;
+            }
+        }
+
+        //if (horizontal < 0) spriteRenderer.flipX = true;
+        //else if (horizontal > 0) spriteRenderer.flipX = false;
+
+        if (horizontal != 0)
+        {
+            Quaternion rot = new Quaternion(0, horizontal > 0 ? -180 : 0, 0, 0);
+
+            if (horizontal < 0) spriteRenderer.transform.localRotation = rot;
+            else if (horizontal > 0) spriteRenderer.transform.localRotation = rot;
+
+            //this.GetComponent<PlayerManager>().isFlipped = spriteRenderer.flipX;
+            this.GetComponent<PlayerManager>().isFlipped = horizontal > 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) isGrounded = true;
+    }
+
+    IEnumerator IsDead()
+    {
+        playerManager.animator.SetBool("isDead", true);
+
+        yield return new WaitForSeconds(0.25f);
+
+        playerManager.animator.SetBool("isDead", false);
+        playerManager.animator.SetBool("stayDead", true);
+
+        this.enabled = false;
+    }
+}
+
